53d0bc6fa5108a05c003fd142a60d9d646644d8e
Onkar2527 <onkardesai2527@gmail.com>
2026-08-14 18:56:10 +0530

    ok all

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
 
   //apiBaseUrl: 'http://kpibackend.kredpool.in/api'
-  apiBaseUrl: 'http://192.168.31.147:3000/api',
   //apiBaseUrl: 'http://[IP_ADDRESS]/api',
-};
+  apiBaseUrl: 'http://localhost:3000/api',
+};  
